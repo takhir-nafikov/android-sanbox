987210ea0d8167cccbbbe8782156a65d55698424
takhir-nafikov <takhir.learn@gmail.com>
2020-10-27 16:18:19 +0300

    simple service

---
+++ service/src/main/java/com/takhir/service/MainActivity.java
@@ -0,0 +1,24 @@
+package com.takhir.service;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+
+  public void onClickStart(View v) {
+    startService(new Intent(this, MyService.class));
+  }
+
+  public void onClickStop(View v) {
+    stopService(new Intent(this, MyService.class));
+  }
+}
