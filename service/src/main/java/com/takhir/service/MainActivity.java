7ae4a1cdf4d3b1345b0c5c90adc3d229accaca60
takhir-nafikov <takhir.learn@gmail.com>
2020-10-27 18:08:32 +0300

    связь с сервисом через биндер

--- service/src/main/java/com/takhir/service/MainActivity.java
+++ service/src/main/java/com/takhir/service/MainActivity.java
@@ -2,12 +2,12 @@ package com.takhir.service;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
-import android.content.BroadcastReceiver;
-import android.content.Context;
+
+import android.content.ComponentName;
 import android.content.Intent;
-import android.content.IntentFilter;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
@@ -16,105 +16,62 @@ public class MainActivity extends AppCompatActivity {
 
   final String LOG_TAG = "myLogs";
 
-  final int TASK1_CODE = 1;
-  final int TASK2_CODE = 2;
-  final int TASK3_CODE = 3;
-
-  public final static int STATUS_START = 100;
-  public final static int STATUS_FINISH = 200;
-
-  public final static String PARAM_TIME = "time";
-  public final static String PARAM_TASK = "task";
-  public final static String PARAM_RESULT = "result";
-  public final static String PARAM_STATUS = "status";
-
-  public final static String BROADCAST_ACTION = "action_br";
-
-  TextView tvTask1;
-  TextView tvTask2;
-  TextView tvTask3;
-  BroadcastReceiver br;
+  boolean bound = false;
+  ServiceConnection sConn;
+  Intent intent;
+  MyService myService;
+  TextView tvInterval;
+  long interval;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    tvTask1 = (TextView) findViewById(R.id.tvTask1);
-    tvTask1.setText("Task1");
-    tvTask2 = (TextView) findViewById(R.id.tvTask2);
-    tvTask2.setText("Task2");
-    tvTask3 = (TextView) findViewById(R.id.tvTask3);
-    tvTask3.setText("Task3");
 
-    // создаем BroadcastReceiver
-    br = new BroadcastReceiver() {
-      // действия при получении сообщений
-      public void onReceive(Context context, Intent intent) {
-        int task = intent.getIntExtra(PARAM_TASK, 0);
-        int status = intent.getIntExtra(PARAM_STATUS, 0);
-        Log.d(LOG_TAG, "onReceive: task = " + task + ", status = " + status);
+    tvInterval = (TextView) findViewById(R.id.tvInterval);
+    intent = new Intent(this, MyService.class);
+    sConn = new ServiceConnection() {
 
-        // Ловим сообщения о старте задач
-        if (status  == STATUS_START) {
-          switch (task) {
-            case TASK1_CODE:
-              tvTask1.setText("Task1 start");
-              break;
-            case TASK2_CODE:
-              tvTask2.setText("Task2 start");
-              break;
-            case TASK3_CODE:
-              tvTask3.setText("Task3 start");
-              break;
-          }
-        }
+      public void onServiceConnected(ComponentName name, IBinder binder) {
+        Log.d(LOG_TAG, "MainActivity onServiceConnected");
+        myService = ((MyService.MyBinder) binder).getService();
+        bound = true;
+      }
 
-        // Ловим сообщения об окончании задач
-        if (status == STATUS_FINISH) {
-          int result = intent.getIntExtra(PARAM_RESULT, 0);
-          switch (task) {
-            case TASK1_CODE:
-              tvTask1.setText("Task1 finish, result = " + result);
-              break;
-            case TASK2_CODE:
-              tvTask2.setText("Task2 finish, result = " + result);
-              break;
-            case TASK3_CODE:
-              tvTask3.setText("Task3 finish, result = " + result);
-              break;
-          }
-        }
+      public void onServiceDisconnected(ComponentName name) {
+        Log.d(LOG_TAG, "MainActivity onServiceDisconnected");
+        bound = false;
       }
     };
-    // создаем фильтр для BroadcastReceiver
-    IntentFilter intFilt = new IntentFilter(BROADCAST_ACTION);
-    // регистрируем (включаем) BroadcastReceiver
-    registerReceiver(br, intFilt);
   }
 
   @Override
-  protected void onDestroy() {
-    super.onDestroy();
-    // дерегистрируем (выключаем) BroadcastReceiver
-    unregisterReceiver(br);
+  protected void onStart() {
+    super.onStart();
+    bindService(intent, sConn, 0);
+  }
+
+  @Override
+  protected void onStop() {
+    super.onStop();
+    if (!bound) return;
+    unbindService(sConn);
+    bound = false;
   }
 
   public void onClickStart(View v) {
-    Intent intent;
-
-    // Создаем Intent для вызова сервиса,
-    // кладем туда параметр времени и код задачи
-    intent = new Intent(this, MyService.class).putExtra(PARAM_TIME, 7)
-        .putExtra(PARAM_TASK, TASK1_CODE);
-    // стартуем сервис
     startService(intent);
+  }
 
-    intent = new Intent(this, MyService.class).putExtra(PARAM_TIME, 4)
-        .putExtra(PARAM_TASK, TASK2_CODE);
-    startService(intent);
+  public void onClickUp(View v) {
+    if (!bound) return;
+    interval = myService.upInterval(500);
+    tvInterval.setText("interval = " + interval);
+  }
 
-    intent = new Intent(this, MyService.class).putExtra(PARAM_TIME, 6)
-        .putExtra(PARAM_TASK, TASK3_CODE);
-    startService(intent);
+  public void onClickDown(View v) {
+    if (!bound) return;
+    interval = myService.downInterval(500);
+    tvInterval.setText("interval = " + interval);
   }
 }
--- service/src/main/java/com/takhir/service/MyService.java
+++ service/src/main/java/com/takhir/service/MyService.java
@@ -1,85 +1,64 @@
 package com.takhir.service;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 import android.util.Log;
 
 public class MyService extends Service {
 
   final String LOG_TAG = "myLogs";
-  ExecutorService es;
+
+  MyBinder binder = new MyBinder();
+
+  Timer timer;
+  TimerTask tTask;
+  long interval = 1000;
 
   public void onCreate() {
     super.onCreate();
     Log.d(LOG_TAG, "MyService onCreate");
-    es = Executors.newFixedThreadPool(2);
+    timer = new Timer();
+    schedule();
   }
 
-  public void onDestroy() {
-    super.onDestroy();
-    Log.d(LOG_TAG, "MyService onDestroy");
+  void schedule() {
+    if (tTask != null) tTask.cancel();
+    if (interval > 0) {
+      tTask = new TimerTask() {
+        public void run() {
+          Log.d(LOG_TAG, "run");
+        }
+      };
+      timer.schedule(tTask, 1000, interval);
+    }
   }
 
-  public int onStartCommand(Intent intent, int flags, int startId) {
-    Log.d(LOG_TAG, "MyService onStartCommand");
+  long upInterval(long gap) {
+    interval = interval + gap;
+    schedule();
+    return interval;
+  }
 
-    int time = intent.getIntExtra(MainActivity.PARAM_TIME, 1);
-    int task = intent.getIntExtra(MainActivity.PARAM_TASK, 0);
-
-    MyRun mr = new MyRun(startId, time, task);
-    es.execute(mr);
-
-    return super.onStartCommand(intent, flags, startId);
+  long downInterval(long gap) {
+    interval = interval - gap;
+    if (interval < 0) interval = 0;
+    schedule();
+    return interval;
   }
 
   public IBinder onBind(Intent arg0) {
-    return null;
+    Log.d(LOG_TAG, "MyService onBind");
+    return binder;
   }
 
-  class MyRun implements Runnable {
-
-    int time;
-    int startId;
-    int task;
-
-    public MyRun(int startId, int time, int task) {
-      this.time = time;
-      this.startId = startId;
-      this.task = task;
-      Log.d(LOG_TAG, "MyRun#" + startId + " create");
-    }
-
-    public void run() {
-      Intent intent = new Intent(MainActivity.BROADCAST_ACTION);
-      Log.d(LOG_TAG, "MyRun#" + startId + " start, time = " + time);
-      try {
-        // сообщаем о старте задачи
-        intent.putExtra(MainActivity.PARAM_TASK, task);
-        intent.putExtra(MainActivity.PARAM_STATUS, MainActivity.STATUS_START);
-        sendBroadcast(intent);
-
-        // начинаем выполнение задачи
-        TimeUnit.SECONDS.sleep(time);
-
-        // сообщаем об окончании задачи
-        intent.putExtra(MainActivity.PARAM_STATUS, MainActivity.STATUS_FINISH);
-        intent.putExtra(MainActivity.PARAM_RESULT, time * 100);
-        sendBroadcast(intent);
-
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
-      stop();
-    }
-
-    void stop() {
-      Log.d(LOG_TAG, "MyRun#" + startId + " end, stopSelfResult("
-          + startId + ") = " + stopSelfResult(startId));
+  class MyBinder extends Binder {
+    MyService getService() {
+      return MyService.this;
     }
   }
 }
